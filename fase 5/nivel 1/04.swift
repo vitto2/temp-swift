a447e8f95fa20451cf0d7619b08dddee321d70d2
Vitor <vitor2071@gmail.com>
2025-12-25 19:54:53 -0300

    feat: implementa desafio 04 nivel 1

---
+++ fase 5/nivel 1/04.swift
@@ -0,0 +1,24 @@
+// Enum AlertLevel
+//  (Low, Medium, High). Use associated values para passar uma mensagem customizada e imprima-a usando switch
+// .
+
+
+enum AlertLevel { 
+  case low(messageLow: String)
+  case medium(messageMedium: String)
+  case high(messageHigh: String)
+}
+
+let lowAlert = AlertLevel.low(messageLow: "Alert low!!")
+let mediumAlert = AlertLevel.medium(messageMedium: "Alert Medium!!")
+let highAlert = AlertLevel.high(messageHigh: "Alert high!!")
+
+
+switch highAlert { 
+  case .low(let message):
+        print(message)
+  case .medium(let message):
+        print(message)
+  case .high(let message):
+        print(message)
+}
